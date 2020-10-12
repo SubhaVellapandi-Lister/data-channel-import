3ef06cbe4d7efa80dbd5c267de14621994c18039
Matt Demaray <matt.demaray@hobsons.com>
2020-10-12 13:11:17 -0700

    add sfdev

--- builtins/deploy.js
+++ builtins/deploy.js
@@ -5,7 +5,15 @@ const environments = {
         profile: 'plat-sandbox',
         region: 'us-east-1',
         bucket: 'ss-data-channels-processor-deployments-platsandbox',
-        stack: 'ss-data-channels-builtin-processor'
+        stack: 'ss-data-channels-builtin-processor',
+        template: 'template-platform.yml'
+    },
+    sfdev: {
+        profile: 'sfdev',
+        region: 'us-east-1',
+        bucket: 'ss-data-channels-processor-deployments-sfdev',
+        stack: 'ss-data-channels-builtin-processor',
+        template: 'template-sfdev.yml'
     }
 }
 
@@ -16,10 +24,10 @@ if (!environments[enviro]) {
     console.error(`could not find enviro config`);
     process.exit(1);
 }
-const { profile, region, bucket, stack } = environments[enviro];
+const { profile, region, bucket, stack, template } = environments[enviro];
 
 execSync('npm run build', { stdio: 'inherit' });
-execSync('npx sam build --template-file template-platform.yml', { stdio: 'inherit' });
+execSync(`npx sam build --template-file ${template}`, { stdio: 'inherit' });
 execSync(`npx sam package --output-template-file packaged.yml --s3-bucket ${bucket} --profile ${profile}`, { stdio: 'inherit' });
 execSync(`npx sam deploy --template-file packaged.yml --stack-name ${stack} --capabilities CAPABILITY_IAM --region ${region} --profile ${profile}`)
 
